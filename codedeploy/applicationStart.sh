fbce0386cd7bc2b3475aca984b56166486f5e94c
AshwinKumarRk <rameshkumar.a@northeastern.edu>
2021-11-05 20:56:30 -0400

    Test run final 4

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -3,7 +3,7 @@ cd /home/ubuntu/webapp/
 unzip -o webapp.zip
 npm i
 sudo npm i nodemon -g
-sudo npm install pm2
+sudo npm install pm2 -g
 sudo pm2 stop all
 sudo pm2 start app.js
 # nohup npm start &

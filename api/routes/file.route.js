d8e5e47bf212f7049370f62484bcefa8de7eb770
AshwinKumarRk <rameshkumar.a@northeastern.edu>
2021-11-11 13:15:46 -0500

    Update File Structure

--- api/routes/file.route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const router = Router()
-const files = require('../controllers/file.controller')
-
-router.post('/self/pic', files.createFile)
-router.delete('/self/pic', files.deleteFile)
-router.get('/self/pic', files.findFile)
-
-module.exports = router
--- codedeploy/applicationStop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# cd /home/ubuntu/webapp/
-# sudo killall node
